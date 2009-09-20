a0a7ba8206e0ff52587459231d3df954aada004c
yugo <yugo@faith>
2009-09-21 00:31:23 +0900

    layered issue info view #3

--- app/controllers/version_gantt_chart_controller.rb
+++ app/controllers/version_gantt_chart_controller.rb
@@ -1,5 +1,30 @@
 class VersionGanttChartController < ApplicationController
 
   def index
+    #ユーザの担当するIssueをVersionごとに集める
+    user_version_issue_hash = {}
+
+    user_list = User.find(:all)
+    user_list.each do |user|
+      version_issue_hash = {}
+
+      user.projects.each do |project|
+        project.versions.each do |version|
+          assigned_issue_list = []
+          version.fixed_issues.each do |issue|
+            if issue.assigned_to == user
+              assigned_issue_list.push issue
+            end
+          end
+          version_issue_hash[version] = assigned_issue_list unless assigned_issue_list.empty?
+        end
+      end
+
+      user_version_issue_hash[user] = version_issue_hash unless version_issue_hash.empty?
+    end
+
+    #Versionの期日が早い順にソート
+    @sorted_user_version_issue_hash =  user_version_issue_hash
+
   end
 end
--- init.rb
+++ init.rb
@@ -5,4 +5,5 @@ Redmine::Plugin.register :redmine_version_gantt_chart do
   author 'Yugo Hasegawa'
   description '作業者ごとバージョンごとに進捗状況をガントチャート風に表示するプラグインです'
   version '0.0.1'
+  menu :application_menu, :version_gantt_chart, {:controller => 'version_gantt_chart', :action => 'index'}, :caption => 'バージョンガントチャート', :last => true
 end
