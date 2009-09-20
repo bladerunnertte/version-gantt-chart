f77c3cf6b23f63f407641f56c619fad46d5e0838
yugo <yugo@faith>
2009-09-20 11:41:19 +0900

    edit description

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ require 'redmine'
 
 Redmine::Plugin.register :redmine_version_gantt_chart do
   name 'Redmine Version Gantt Chart plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Yugo Hasegawa'
+  description '作業者ごとバージョンごとに進捗状況をガントチャート風に表示するプラグインです'
   version '0.0.1'
 end
