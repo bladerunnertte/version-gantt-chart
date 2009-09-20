560621f8af1930d7fa0af219319a804a2b6bda9c
yugo <yugo@faith>
2009-09-20 11:31:01 +0900

    generate plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_version_gantt_chart do
+  name 'Redmine Version Gantt Chart plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
