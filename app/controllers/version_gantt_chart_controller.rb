6f1fac90bfccd3298b5d24970060b0546d0eab2b
yugo <yugo@faith>
2009-09-27 19:17:35 +0900

    showing only membered project

--- app/controllers/version_gantt_chart_controller.rb
+++ app/controllers/version_gantt_chart_controller.rb
@@ -10,12 +10,14 @@ class VersionGanttChartController < ApplicationController
     user_list = User.find(:all)
     user_list.each do |user|
       user.projects.each do |project|
-        project.versions.each do |version|
-          user_tasks = UserTasks.new( user, version )
-          version.fixed_issues.each do |issue|
-            user_tasks.add issue
+        if User.current.admin? || User.current.member_of?(project)
+          project.versions.each do |version|
+            user_tasks = UserTasks.new( user, version )
+            version.fixed_issues.each do |issue|
+              user_tasks.add issue
+            end
+            events.push( user_tasks ) unless user_tasks.empty?
           end
-          events.push( user_tasks ) unless user_tasks.empty?
         end
       end
     end
