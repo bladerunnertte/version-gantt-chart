3f53eaf5fdcf587deaefd78ddff72e1e81656bb6
yugo <yugo@faith>
2009-10-03 23:37:43 +0900

    GPL???????????????????????????????

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ require 'redmine'
 Redmine::Plugin.register :redmine_version_gantt_chart do
   name 'Redmine Version Gantt Chart plugin'
   author 'Yugo Hasegawa'
-  description '作業者ごとバージョンごとに進捗状況をガントチャート風に表示するプラグインです'
-  version '0.0.1'
-  menu :top_menu, :version_gantt_chart, {:controller => 'version_gantt_chart', :action => 'index'}, :caption => 'バージョンガントチャート', :last => true
+  description 'This plugin displays workload and progress of version.'
+  version '0.1'
+  menu :top_menu, :version_gantt_chart, {:controller => 'version_gantt_chart', :action => 'index'}, :caption => :label_menu, :last => true
 end
