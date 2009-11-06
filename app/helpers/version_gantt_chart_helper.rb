3c30eadb2eeb3f99314403ffd9573eb93ae3ddc0
yugo <yugo@faith>
2009-11-06 14:05:39 +0900

    UserTask??????????????

--- app/helpers/version_gantt_chart_helper.rb
+++ app/helpers/version_gantt_chart_helper.rb
@@ -1,2 +1,8 @@
 module VersionGanttChartHelper
+  def link_to_usertask( usertask )
+    link_text = "#{link_to_version_unless_nil(usertask.version)}"
+    link_text << " - "
+    link_text << "#{link_to_project(usertask.version.project)}"
+    link_text << "(#{usertask.count_issues.to_s})"
+  end
 end
