8fe0b9f4c5c81ffee0e41deb0f19b008184cb613
yugo <yugo@faith>
2010-03-26 17:18:00 +0900

    ??????????

--- init.rb
+++ init.rb
@@ -4,6 +4,6 @@ Redmine::Plugin.register :redmine_version_gantt_chart do
   name 'Redmine Version Gantt Chart plugin'
   author 'Yugo Hasegawa'
   description 'This plugin displays workload and progress of version.'
-  version '0.5.0'
+  version '0.5.1'
   menu :top_menu, :version_gantt_chart, {:controller => 'version_gantt_chart', :action => 'index'}, :caption => :label_menu, :last => true
 end
